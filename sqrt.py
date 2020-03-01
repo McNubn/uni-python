cd5016e2789c5ac76962b8cbbb16b214900bccd8
Brian Doheny <brian.doheny@shopify.com>
2020-03-01 16:00:44 +0000

    Week 6 now done with a working sqrt function - had to turn to the internet for it

--- sqrt.py
+++ sqrt.py
@@ -1,18 +1,23 @@
 # Brian Doheny
-# Attempting to create a function that utilizes Newton's method for calculating square roots
-# My understanding here is based off a 2 hour chat with a Maths graduate. Fingers crossed!
+# I initially attempted this myself by first understandind the algorithm and trying to work it out in Python.
+# My own attempts at writing this function failed, and so I had to turn to a pre-written function, and studying that instead.
+# I have been using the outline found at: https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo
 
-difference = 0.001
+# Define the fuction
+def sqrt(x):
+# Need to turn x into a new variable that we can run the algorithm on. i.e. a variable we can change now, so we can compare back to original later.
+    r = x
+# Precision allows us to decide what range of accuracy we want here.
+# In this case it's to the power of minus 10, so accuracy down to 0.000000001
+    precision = 10 ** (-10)
 
-def recursive(x):
-    y = square_root(x)
-
-    while (y * y > (x + difference)) and (y * y < (x - difference)):
-        x = y
-        recursive(x)
-    else:
-        print(y)
-
-        
-def square_root(x):
-  x = x - (x * x)/(2 * x)
+# Can use abs so that we're only dealing with the positive number here. 
+# Makes the next step more simple, as we can ignore the negative square root.
+# Here we're squaring our "r" figure and subtracting from x. If the result is greater than precision, than r cannot be the square root.
+    while abs(x - r * r) > precision:
+# This will run if the above is true, and so "r" is not the square root.
+# We therefore change r to a new, smaller figure for the next attempt. In this case adding r to x, diving that by r, and then dividing that by 2.
+# This gives us a new smaller r to try again with. This will keep going on until x-r*r is accurate to 0.000000001 
+        r = (r + x / r) / 2
+# When the program finds an r that is accurate to 0.000000001, the function will return it for use in remainder of the main program.
+    return r
--- week6-squareroot.py
+++ week6-squareroot.py
@@ -2,15 +2,15 @@
 # Write a program that takes a positive floating-point number as input and 
 # outputs an approximation of its square root.
 
-# Will use the Math module for now until I clarify if I need to make my own function here.
-import math
+# Now importing the sqrt function from the sqrt file.
+from sqrt import sqrt
 
 # Asking the user to input a floating point number.
 x = float(input("Please enter a positive number: "))
 
-# Take their result and run the math.sqrt function on it.
-result = math.sqrt(x)
-# Rounding the answer to 2 decimal places - makes it a bit tider.
-result = round(result,2)
+# Take their result and run the sqrt function on it.
+result = sqrt(x)
+# Rounding the answer to 1 decimal places - makes it a bit tidier, and brings it inline with sample answer.
+result = round(result,1)
 # And print the result, letting the user know their initial input and the square root of it.
 print("The square root of {} is approx. {}.".format(x,result))
