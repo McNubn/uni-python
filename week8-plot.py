c2c0e36a6096020c2d4724c52f3aa3af329ab880
Brian Doheny <brian.doheny@shopify.com>
2020-03-15 08:44:56 +0000

    Week 8 homework done. Three plots one 1 axes, along with titles, etc.

--- week8-plot.py
+++ week8-plot.py
@@ -2,14 +2,25 @@
 # Write a program that displays a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 
 # in the range [0, 4] on the one set of axes.
 
+# Improting pyplot for the plots themselves. Numpy to create an array to square and cube.
 from matplotlib import pyplot as plt
 import numpy as np
 
+# Letting numpy create the array. Set the higher limit at 4.1 so that 4.0 will be included.
+# Got this tip from https://realpython.com/how-to-use-numpy-arange/#providing-all-range-arguments
 numbers = np.arange(0, 4.1, 0.5)
+# Squaring and cubing the array, and saving them to variables for plotting.
 g = numbers ** 2
 h = numbers ** 3
 
-plt.plot(numbers,numbers)
-plt.plot(numbers, g)
-plt.plot(numbers, h)
+plt.plot(numbers,numbers, label = "f(x)=x")
+# Found out how to format the squared and cubed characters via https://www.fileformat.info/info/unicode/char/b2/index.htm
+plt.plot(numbers, g, label = "g(x)=x" +  u"\u00B2")
+plt.plot(numbers, h, label = "h(x)=x" + u"\u00B3")
+# Now to add labels and titles to make it as clear as possible.
+plt.xlabel("Value of x")
+plt.ylabel("Result of function")
+plt.title("Results of f(x)=x, g(x)=x" + u"\u00B2" + " and h(x)=x" + u"\u00B3")
+# Adding a legend that'll utilise the labels for each plot that I set above.
+plt.legend()
 plt.show()
